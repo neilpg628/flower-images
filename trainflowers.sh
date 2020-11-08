7f3361688ee668a6e8f9e4c4a9a7f5fa4dafe7a9
Neil Pramod Gupta <guptan4@digs>
2020-11-07 18:20:25 -0800

    Edited GPU

--- trainflowers.sh
+++ trainflowers.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 #SBATCH -p gpu
-#SBATCH --mem=60g
-#SBATCH -c 8
-#SBATCH --gres=gpu:gtx1080ti:2
-#SBATCH -o flowerjob.log
+#SBATCH --mem=32g
+#SBATCH -c 6
+#SBATCH --gres=gpu:titan:1
+#SBATCH -o flowerjobtitan.log
 
 
 source activate /software/conda/envs/tensorflow
 
 nvidia-smi
 
-python3 train.py --outdir=./training-runs --gpus=2 --data=./datasets/flowers --mirror=1 --metrics none --cfg=auto -n
+python3 train.py --outdir=./training-runs --gpus=1 --data=./datasets/flowers --mirror=1 --metrics none --cfg=auto -n
 
 nvidia-smi
 
-python3 train.py --outdir=./training-runs --gpus=2 --data=./datasets/flowers --mirror=1 --metrics none --cfg=auto
+python3 train.py --outdir=./training-runs --gpus=1 --data=./datasets/flowers --mirror=1 --metrics none --cfg=auto
 
 nvidia-smi
 
